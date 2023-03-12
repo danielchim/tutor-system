43d5ad5be58d08e920ea4f348cdd4c916695536b
danielchim <kahungchim@gmail.com>
2023-03-13 02:21:55 +0800

    inital commit

---
+++ frontend/config/site.ts
@@ -0,0 +1,45 @@
+import { NavItem } from "@/types/nav"
+
+interface SiteConfig {
+  name: string
+  description: string
+  mainNav: NavItem[]
+  links: {
+    twitter: string
+    github: string
+    docs: string
+  }
+}
+
+export const siteConfig: SiteConfig = {
+  name: "NextJobs",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+    {
+      title: "Jobs",
+      href: "/",
+    },
+    {
+      title: "Employer zone",
+      href: "/",
+    },
+    {
+      title: "Company",
+      href: "/",
+    },
+    {
+      title: "Admin",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
