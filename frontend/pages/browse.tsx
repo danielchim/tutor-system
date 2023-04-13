0a41272c8131277d82021e5ae73c2fc5547b0069
imytdotio <imytdotio@gmail.com>
2023-04-13 17:00:50 +0800

    skillTag :)

--- frontend/pages/browse.tsx
+++ frontend/pages/browse.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Layout } from '@/components/layout';
+import React, {useState} from 'react';
+import {Layout} from '@/components/layout';
 import {useSession} from "next-auth/react";
 import useSWR from 'swr'
 import fetcher from "@/lib/fetcher";
@@ -12,17 +12,18 @@ interface Job {
   location: string;
   salary: string;
   description: string;
+  skillList: object;
 }
+
 
 const Browse = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [location, setLocation] = useState('');
   const [selectedJob, setSelectedJob] = useState<Job | null>(null);
 
-  const session= useSession().data;
+  const session = useSession().data;
   const {data, error} = useSWR(['http://localhost:8080/api/jobs/'], fetcher);
   console.log(data)
-
   if (error) {
     return (
       <Layout>
@@ -34,17 +35,29 @@ const Browse = () => {
   }
   const jobs = data?.map((job: any) => {
     return {
-      id:job.idJobs,
+      id: job.idJobs,
       title: job.name,
       company: job.company.name,
-      description: job.description
+      description: job.description,
+      skillList: job.skills,
     };
   });
 
 
-  const handleJobClick = (job: Job) => {
-    setSelectedJob(job);
-  };
+  const SkillTag = ({title, idSkill}) => {
+    return <p className='rounded px-2 bg-zinc-200/80 w-fit inline-block mr-2' onClick={() => {
+      console.log(idSkill)
+      console.log(title)
+    }}>{title}</p>
+
+  }
+
+  const handleJobClick = (
+      job: Job
+    ) => {
+      setSelectedJob(job);
+    }
+  ;
 
   const handleCloseClick = () => {
 
@@ -69,29 +82,38 @@ const Browse = () => {
                   >
                     <h3 className="text-lg font-medium mb-2">{job.title}</h3>
                     <p className="text-gray-700">{job.company}</p>
+                    {console.log(job.title, job.skillList)}
+                    {job.skillList != null ? job.skillList.map((skill) => {
+                      console.log('s', skill)
+                      return <SkillTag title={skill.name} idSkill={skill.idSkills}/>
+                    }) : <></>}
                   </div>
                 </>
 
               ))}
             </div>
             <div className="w-3/4">
-              {selectedJob? (
+              {selectedJob ? (
                 <div className="bg-white rounded-lg shadow py-6 px-8">
                   <h3 className="text-2xl font-medium mb-2">{selectedJob.title}</h3>
                   <p className="text-gray-700 mb-2">{selectedJob.company}</p>
                   <p className="text-gray-700 mb-2">{selectedJob.description}</p>
+                  {selectedJob.skillList != null ? selectedJob.skillList.map((skill) => {
+                    return <SkillTag title={skill.name}/>
+                  }) : <></>}
                 </div>
-              ):(
+              ) : (
                 <div className="bg-white rounded-lg shadow py-6 px-8">
                   <h3 className="text-2xl font-medium mb-2">Select a job on the left to get started</h3>
                 </div>
-                )
+              )
               }
             </div>
           </div>
         </div>
       </section>
     </Layout>
-  );
+  )
+    ;
 }
 export default Browse;
